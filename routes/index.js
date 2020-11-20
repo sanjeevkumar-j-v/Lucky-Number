aca565d329d736b146d0978d40e03abb24ecdb8d
sanjeevkumar-j-v <sanjeevkumarjv@gmail.com>
2020-11-20 12:47:56 +0530

    Show answers

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
+  res.render("answers", {
+    title: "Answers",
+  });
+});
+router.get("/dares", function (req, res, next) {
   res.render("index", {
     title: "Lucky Level Number",
   });
